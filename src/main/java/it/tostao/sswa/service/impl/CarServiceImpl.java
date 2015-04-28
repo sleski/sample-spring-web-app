e2141ac622cd46d04c7340fc93a36afed77e5fcb
Slawek Leski <slawomir.leski@gmail.com>
2015-04-28 23:15:47 +0200

    Project updated, hsqldb to fix

--- src/main/java/it/tostao/sswa/service/impl/CarServiceImpl.java
+++ src/main/java/it/tostao/sswa/service/impl/CarServiceImpl.java
@@ -2,6 +2,10 @@ package it.tostao.sswa.service.impl;
 
 import it.tostao.sswa.model.Car;
 import it.tostao.sswa.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
@@ -9,9 +13,18 @@ import java.util.List;
  * Created by sleski on 28.04.2015.
  */
 public class CarServiceImpl implements CarService {
+
+    Logger LOG = LoggerFactory.getLogger(CarServiceImpl.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public int sizeInGarage() {
-        return 0;
+        String query = "SELECT count(*) FROM car";
+        int counter = jdbcTemplate.queryForObject(query, Integer.class);
+        LOG.info("No of cars = " + counter);
+        return counter;
     }
 
     @Override
