bb1eb182eac81cbd7df83532e15abcd750cf6e98
Slawek Leski <slawomir.leski@gmail.com>
2015-04-30 21:29:47 +0200

    updated logger

---
+++ src/test/java/it/tostao/sswa/service/CarServiceImplTest.java
@@ -0,0 +1,23 @@
+package it.tostao.sswa.service;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Created by sleski on 30.04.2015.
+ */
+public class CarServiceImplTest {
+
+    Logger LOG = LoggerFactory.getLogger(CarServiceImplTest.class);
+
+    /**
+     * Test cars counter.
+     */
+    @Test
+    public void checkCounter(){
+
+    }
+}
