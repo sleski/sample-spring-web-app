1a31d946e9615997c0bd7b3d7c246a48f69e0c08
Slawek Leski <slawomir.leski@gmail.com>
2015-05-02 00:31:02 +0200

    dbunit updated.

--- src/test/java/it/tostao/sswa/service/CarServiceImplTest.java
+++ src/test/java/it/tostao/sswa/service/CarServiceImplTest.java
@@ -1,5 +1,6 @@
 package it.tostao.sswa.service;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import it.tostao.sswa.service.impl.CarServiceImpl;
 import org.junit.Before;
@@ -10,14 +11,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /**
  * Created by sleski on 30.04.2015.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/it/tostao/sswa/sswai-test.xml"})
-
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class})
 public class CarServiceImplTest {
 
     Logger LOG = LoggerFactory.getLogger(CarServiceImplTest.class);
